22b6b5b2e8013848025e0a11affd5b674439c960
DiogoBarrosMartins <diogobarrosmartins@hotmail.com>
2025-09-15 01:33:39 +0100

    modimp

--- src/player/player.module.ts
+++ src/player/player.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PlayerService } from './player.service';
 import { PlayerController } from './player.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { VillageModule } from '../village/village.module';
 
 @Module({
+  imports: [VillageModule], 
   controllers: [PlayerController],
   providers: [PlayerService, PrismaService],
 })
